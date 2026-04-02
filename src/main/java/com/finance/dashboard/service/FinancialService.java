43a437d48c168762028d6bee2ffb141dc74528a7
Mrityunjay Kumar <mrityunjaykmr.mk@gmail.com>
2026-04-02 18:33:02 +0530

    Service added

--- src/main/java/com/finance/dashboard/service/FinancialService.java
+++ src/main/java/com/finance/dashboard/service/FinancialService.java
@@ -1,7 +1,10 @@
 package com.finance.dashboard.service;
 
 import com.finance.dashboard.entity.FinancialRecord;
+import com.finance.dashboard.enums.Category;
+import com.finance.dashboard.enums.RecordType;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface FinancialService {
@@ -10,5 +13,11 @@ public interface FinancialService {
     public List<FinancialRecord> getAllRecords();
     public FinancialRecord getRecordById(Long id);
     public FinancialRecord updateRecord(Long id, FinancialRecord updatedRecord);
+    public List<FinancialRecord> filterRecords(
+            RecordType type,
+            Category category,
+            LocalDate startDate,
+            LocalDate endDate
+    );
     public void deleteRecord(Long id);
 }
--- src/main/java/com/finance/dashboard/service/serviceImpl/FinancialServiceImpl.java
+++ src/main/java/com/finance/dashboard/service/serviceImpl/FinancialServiceImpl.java
@@ -1,10 +1,13 @@
 package com.finance.dashboard.service.serviceImpl;
 
 import com.finance.dashboard.entity.FinancialRecord;
+import com.finance.dashboard.enums.Category;
+import com.finance.dashboard.enums.RecordType;
 import com.finance.dashboard.repository.FinancialRecordRepository;
 import com.finance.dashboard.service.FinancialService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -46,6 +49,28 @@ public class FinancialServiceImpl implements FinancialService {
         record.setDescription(updatedRecord.getDescription());
 
         return financialRecordRepository.save(record);
+    }
+
+    @Override
+    public List<FinancialRecord> filterRecords(RecordType type, Category category, LocalDate startDate, LocalDate endDate) {
+
+        if (type != null && category != null && startDate != null && endDate != null) {
+            return financialRecordRepository.findByTypeCategoryDate(type, category, startDate, endDate);
+        }
+
+        if (type != null) {
+            return financialRecordRepository.findByType(type);
+        }
+
+        if (category != null) {
+            return financialRecordRepository.findByCagtegory(category);
+        }
+
+        if (startDate != null && endDate != null) {
+            return financialRecordRepository.findByDateRange(startDate, endDate);
+        }
+
+        return financialRecordRepository.findAll();
     }
 
     @Override
